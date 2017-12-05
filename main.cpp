d410c5b5018e82d234ba4179aa66ee29e5646d1d
vabon <bucks7abu@gmail.com>
2017-12-05 16:54:35 +0300

    Add logger

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
+#include "lib.h"
 #include <iostream>
+#include <spdlog/spdlog.h>
 
 int main(int argc, const char * argv[])
 {
     std::cout << "Hello, World!" << std::endl;
+
+    auto logger = spdlog::stdout_logger_mt("console");
+	logger->info("version {} was started", version());
     
     return 0;
 }
